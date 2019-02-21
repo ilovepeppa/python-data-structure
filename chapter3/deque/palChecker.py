1367d59099fc56ab4f350883a9d4ef18fc045f7d
ilovepeppa <ilovecode@yeah.net>
2019-02-21 16:17:53 +0800

    chapter3-deque

---
+++ chapter3/deque/palChecker.py
@@ -0,0 +1,21 @@
+from deque import Deque
+
+
+def palchecker(astring):
+    deque = Deque()
+
+    for ch in astring:
+        deque.add_rear(ch)
+
+    equal = True
+    while deque.size() > 1:
+        first = deque.remove_front()
+        last = deque.remove_rear()
+        if first != last:
+            equal = False
+            break
+    return equal
+
+
+print(palchecker("lsdkjfskf"))
+print(palchecker("radar"))
