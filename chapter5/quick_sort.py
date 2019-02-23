66c7061217002489a46fe04ce50852dc4d1383e0
ilovepeppa <ilovecode@yeah.net>
2019-02-23 15:49:53 +0800

    chapter5-quick_sort

--- chapter5/quick_sort.py
+++ chapter5/quick_sort.py
@@ -8,27 +8,31 @@ def quick_sort(alist):
     return quick_sort(left_half) + [alist[0]] + quick_sort(right_half)
 
 
-def quick_sort_v2(alist):
-    if len(alist) <= 1:
+def quick_sort_v2(alist, start, end):
+    if end <= start:
         return
 
-    left = 1
-    right = len(alist) - 1
+    left = start + 1
+    right = end
 
-    while left < right:
-        if alist[left] < alist[0]:
+    while True:
+        while left <= right and alist[left] <= alist[start]:
             left += 1
-        else:
-            alist[left], alist[right] = alist[right], alist[left]
+        while left <= right and alist[right] >= alist[start]:
             right -= 1
 
-    alist[0], alist[right] = alist[right], alist[0]
+        if left > right:
+            break
 
-    # quick_sort_v2(alist[:right])
-    # quick_sort_v2(alist[right + 1:])
+        alist[left], alist[right] = alist[right], alist[left]
+
+    alist[start], alist[right] = alist[right], alist[start]
+
+    quick_sort_v2(alist, start, right - 1)
+    quick_sort_v2(alist, right + 1, end)
 
 
 if __name__ == '__main__':
     alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    quick_sort_v2(alist)
+    quick_sort_v2(alist, 0, len(alist) - 1)
     print(alist)
